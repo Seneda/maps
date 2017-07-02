e5263b9b032342a54859f7c7968a088154ad08fd
Seneda <senedanassirali@gmail.com>
2017-07-02 22:34:55 +0100

    Clean up california data and tidy scripts

--- scripts/parser.py
+++ scripts/parser.py
@@ -5,23 +5,27 @@ Convert locations.json into a usable format that can be read in as a javascript 
 """
 basedir = abspath(join(dirname(__file__), ".."))
 
-locations = json.load(open(join(basedir, "gpsdata/locations.json")))
+locations = json.load(open(join(basedir, "gpsdata/locationsCalifornia.json")))
 
-
+i = 0
 def make_marker(location):
+    global i
     # Convert the maps location entry to a simpler format
     r = {
             'lat': location["latitudeE7"]*1e-7,
             'lng': location["longitudeE7"]*1e-7,
-            'time': location["timestampMs"]
+            'time': location["timestampMs"],
+            'index': i
         }
+    i+=1
     return r
 
-markers = [make_marker(l) for l in locations[27500:39600]]  # Choose a specific subset of my locations.json data
+markers = [make_marker(l) for l in locations[:]]  # Choose a specific subset of my locations.json data
+
 print("{} Markers created".format(len(markers)))
 
 # Save the markers into a file
-filename = join(basedir, "markers","markers.js")
+filename = join(basedir, "markers", "markers.js")
 open(filename, "w")
 file = open(filename, "a")
 file.write("var markers = ")
--- web/map.js
+++ web/map.js
@@ -1,14 +1,31 @@
 function addPoints(map, points){
-//    for (i = 0; i < points.length; i+100000) {
-//        var marker = new google.maps.Marker({
-//           position: points[i],
-//           map: map,
-//           icon: {
-//                path: google.maps.SymbolPath.CIRCLE,
-//                scale: 1,
-//              },
-//           });
-//    }
+    var infowindow =  new google.maps.InfoWindow({
+                content: "info: ",
+                map: map,
+            });
+    infowindow.close()
+    for (i = 0; i < points.length; i+=100) {
+        var marker = new google.maps.Marker({
+           position: points[i],
+           map: map,
+           icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 1,
+              },
+           time: points[i]['time'],
+           index: i.toString()
+           });
+
+        marker.addListener('mouseover', function() {
+            infowindow.setContent(this.index)
+            infowindow.open(map, this);
+        });
+
+//        // assuming you also want to hide the infowindow when user mouses-out
+//        marker.addListener('mouseout', function() {
+//            infowindow.close();
+//        });
+    }
 
 
     var line = new google.maps.Polyline({
